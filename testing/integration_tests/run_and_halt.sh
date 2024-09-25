843d9e2b7598ed4070f9f168008bddcf81f2f1e8
Emma Anholt <emma@anholt.net>
2024-09-25 10:03:48 -0700

    debug-cleanu

--- testing/integration_tests/run_and_halt.sh
+++ testing/integration_tests/run_and_halt.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 set -e
 
-if [ -z "$TEST_DIR" ]; then
-    TEST_DIR=/integration_tests
+if [ -z "$TESTS_DIR" ]; then
+    TESTS_DIR=/integration_tests
 fi
 
-./run.sh
+$TESTS_DIR/run.sh
 halt
