34bbbbefd62136503443fdf04b2563bf03d8c783
Emma Anholt <emma@anholt.net>
2024-11-26 14:38:07 -0800

    binfmt: Align PT_LOAD section offsets down to PAGE_SIZE.
    Page faulting wasn't expecting unaligned offsets, so we wouldn't load
    enough of the file, particularly the section containing PT_DYNAMIC
    (relocations).

--- libs/kerla_utils/alignment.rs
+++ libs/kerla_utils/alignment.rs
@@ -9,3 +9,11 @@ pub const fn align_up(value: usize, align: usize) -> usize {
 pub const fn is_aligned(value: usize, align: usize) -> bool {
     value & (align - 1) == 0
 }
+
+pub const fn align_down_u64(value: u64, align: u64) -> u64 {
+    (value) & !(align - 1)
+}
+
+pub const fn align_up_u64(value: u64, align: u64) -> u64 {
+    align_down_u64(value + align - 1, align)
+}
