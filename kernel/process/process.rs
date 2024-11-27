fd7e5a5973145f14b2f18f319389ba38d6d209c9
Emma Anholt <emma@anholt.net>
2024-11-26 16:44:39 -0800

    binfmt: Round up the ELF mapped VM areas to a page size in length.
    dynamic loading seems to require that we can access bits of the file
    just beyond the mapped size -- plausible, we should be faulting based
    on the page number, not the offset within a page.

--- kernel/process/process.rs
+++ kernel/process/process.rs
@@ -37,7 +37,7 @@ use kerla_runtime::{
     page_allocator::{alloc_pages, AllocPageFlags},
     spinlock::{SpinLock, SpinLockGuard},
 };
-use kerla_utils::alignment::{align_down_u64, align_up};
+use kerla_utils::alignment::{align_down_u64, align_up, align_up_u64};
 
 type ProcessTable = BTreeMap<PId, Arc<Process>>;
 
@@ -610,7 +610,7 @@ fn load_elf(vm: &mut Vm, executable: &Arc<dyn FileLike>, elf: &Elf, offset: u64)
         let vaddr = align_down_u64(load_vaddr, PAGE_SIZE as u64);
         let align_offset = load_vaddr - vaddr;
         let file_size = phdr.p_filesz + align_offset;
-        let mem_size = phdr.p_memsz + align_offset;
+        let mem_size = align_up_u64(phdr.p_memsz + align_offset, PAGE_SIZE as u64) - 1;
         let offset = phdr.p_offset - align_offset;
 
         let area_type = if phdr.p_filesz > 0 {
